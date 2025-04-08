b34d9c6bf37a8709420fc7e3c13a9a262842ca60
Abhik6 <47020279+Abhik6@users.noreply.github.com>
2025-04-08 06:57:50 +0000

    Same Tree Check

--- 10. Binary Trees/same_tree.py
+++ 10. Binary Trees/same_tree.py
@@ -5,4 +5,17 @@ def is_same_tree(p, q):
     :param q: TreeNode -> root of the second tree
     :return: bool -> True if both trees are the same, False otherwise
     """
-    pass
+    
+    if p is None and q is None:
+        return True
+    
+    if (p is None and q is not None) or (q is None and p is not None):
+        return False
+
+    if p.val != q.val:
+        return False
+    
+    is_same = is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right) 
+
+    return is_same
+
