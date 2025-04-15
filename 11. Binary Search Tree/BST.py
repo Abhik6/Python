712b6c068401769b206c77edae0cb6d574bac343
Abhik6 <47020279+Abhik6@users.noreply.github.com>
2025-04-15 14:04:01 +0000

    BST Class

---
+++ 11. Binary Search Tree/BST.py
@@ -0,0 +1,52 @@
+class BSTNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class BST:
+
+    def __init__(self):
+        self.root = None
+
+    def search_helper(self, data, root):
+        if root is None:
+            return False
+        
+        if root.val == data:
+            return True
+        
+        if data < root.val:
+            result = self.search_helper(data, root.left)
+        else:
+            result = self.search_helper(data, root.right)
+
+        return result
+
+    def search(self, data):
+
+        return self.search_helper(data, self.root)
+    
+
+    def insert_helper(self, data, root):
+
+        if root is None:
+            new_node = BSTNode(data)
+            root = new_node
+            return root
+
+        if data < root.val:
+            root.left = self.insert_helper(data, root.left)
+        else:
+            root.right = self.insert_helper(data, root.right)
+
+        return root
+
+
+    def insert(self, data):
+
+        self.root = self.insert_helper(data, self.root)
+
+
+    def delete(self, data):
+        pass
