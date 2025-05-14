4bda333710b5f3897649ef4df829a14550f8111c
Abhik6 <47020279+Abhik6@users.noreply.github.com>
2025-05-14 11:10:52 +0000

    Graph Implementation using Edge List

---
+++ 13. Graphs/graph_implementation_using_edge_list.py
@@ -0,0 +1,29 @@
+class GraphUsingEdgeList:
+    def __init__(self):
+        self.vertices = []
+        self.edges = []
+
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices.append(vertex)
+            return f"Vertex '{vertex}' added to graph"
+        else:
+            return f"Vertex '{vertex}' already present in graph"
+        
+    def add_edge(self, source, destination, weight=1):
+        if source in self.vertices and destination in self.vertices:
+            self.edges.append((source, destination, weight))
+        else:
+            return f" One or both of the vertices not present in graph"
+        
+    def display(self):
+        for vertex in self.vertices:
+            print(vertex)
+        
+        for edge in self.edges:
+            print(edge)
+
+
+
+
+
---
+++ 13. Graphs/test.py
@@ -0,0 +1,14 @@
+from graph_implementation_using_edge_list import *
+
+graph = GraphUsingEdgeList()
+
+graph.add_vertex(1)
+graph.add_vertex(2)
+graph.add_vertex(3)
+graph.add_vertex(4)
+
+graph.add_edge(1,2)
+graph.add_edge(1,3)
+graph.add_edge(2,4)
+
+graph.display()
