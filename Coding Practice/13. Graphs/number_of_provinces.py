2da835f28d189751af19f2a937f9c97b717d2b55
Abhik6 <47020279+Abhik6@users.noreply.github.com>
2025-06-09 10:12:35 +0000

    number of provinces - leetcode

---
+++ Coding Practice/13. Graphs/number_of_provinces.py
@@ -0,0 +1,64 @@
+# Number of Provinces
+# Asked in Companies:
+
+# Quickr
+# Goldman Sachs
+# PhonePe
+# Expedia
+
+
+# Description:
+# You are given n cities, represented by an n x n matrix isConnected, where isConnected[i][j] = 1 if the i-th city and j-th city are directly connected, and isConnected[i][j] = 0 otherwise. A province is defined as a group of cities that are directly or indirectly connected. Your task is to determine the number of provinces (groups of connected cities).
+# A province consists of cities that are connected directly or through a series of connections between other cities. No city outside of this group is connected to the cities within the group.
+
+
+# Input Parameters:
+# isConnected (List[List[int]]): A matrix where isConnected[i][j] = 1 means the i-th city is directly connected to the j-th city, and isConnected[i][j] = 0 means they are not.
+
+# Output:
+# Return the total number of provinces.
+
+
+# Example:
+
+# Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+# Output: 2
+# Explanation: The first two cities are connected, and the third city forms a separate province.
+ 
+# Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+# Output: 3
+# Explanation: Each city is its own province, as there are no connections between cities.
+
+def number_of_provinces(isConnected):
+
+    n = len(isConnected)
+    visited = [False for _ in range(n)]
+    # stack = [0]
+    # visited[0] = True
+
+    count = 0
+    for city in range(n):
+        if not visited[city]:
+            dfs(city, isConnected, visited)
+            count+=1
+
+    return count
+
+def dfs(node, graph, visited=[]):
+    visited[node] = True
+
+    for neighbour in range(len(graph)):
+        if not visited[neighbour]:
+            if graph[node][neighbour] == 1:
+                dfs(neighbour, graph, visited)
+        
+# isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+
+print(number_of_provinces(isConnected))
+
+
+    
+
+
+
